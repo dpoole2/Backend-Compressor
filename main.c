0895e71fa91146bd1b45b3d4c3872363ef891cbe
Dylan <dylanb0708@gmail.com>
2019-05-24 19:04:46 -0400

    This is my second commit with main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//This file should only be editted to implenet the alogrthim
+// Author Dylan Poole
+//
+ 
+int main()
+{
+
+	return 0;//return 1 will mean bad shit happened
+}
